5d2f36ced333011b67bf0707391896e341e2399a
Malakai <41552723+Whomever0@users.noreply.github.com>
2020-04-22 06:22:34 +0100

    Module that holds all the stands and their ids, plus a few utility functions.

---
+++ StandIDs.lua
@@ -0,0 +1,73 @@
+--[[
+
+    Name - name of the script
+    DisplayName - the name that appears in the menu
+    Directory - directory of the stand
+    Stats - stats.. {Power, Speed, Durability}
+    Arrow - bool, obtainable by arrow?
+    Chance - the chance..
+
+]]
+
+local ChanceModule = require('Chances')
+
+local Chances = {
+    ['S'] = 1,
+    ['A'] = 2,
+    ['B'] = 7,
+    ['C'] = 20,
+    ['D'] = 30,
+    ['E'] = 40
+}
+
+-- Directory is a table placeholder for the folder.
+local StandIDs = {
+    [1] = {
+        ['Name'] = "Standless",
+        ['DisplayName'] = 'Standless',
+        ['Directory'] = {},
+        ['Stats'] = {"C","C","C"},
+        ['Arrow'] = false
+    },
+    [2] = {
+        ['Name'] = "StarPlatinum",
+        ['DisplayName'] = 'Star Platinum',
+        ['Directory'] = {},
+        ['Stats'] = {"A","A","A"},
+        ['Arrow'] = true,
+        ['Chance'] = 'A'
+    }
+}
+
+function StandIDs.GetStandOfRank(Rank)
+    local Found = {}
+    for I, V in next, StandIDs do
+        if type(V) == 'table' and (V.Chance and V.Chance:lower() == Rank:lower()) then
+            table.insert(Found, I)
+        end
+    end
+    return Found
+end
+
+function StandIDs.GetId(Name)
+    for I, V in next, StandIDs do
+        if type(V) == 'table' and (V.Name and V.Name:lower() == Name) then
+            return I;
+        end
+    end
+end
+
+function StandIDs.GetRandomStand()
+    local Rank = ChanceModule.Run(Chances)
+    print("Rank: " .. Rank)
+    local Stands = StandIDs.GetStandOfRank(Rank)
+    local EachChance = math.ceil(100 / #Stands) -- math.ceil'ing just incase it returns 99.99..
+    local NewChanceTable = {}
+    for _, V in next, Stands do
+        NewChanceTable[V] = EachChance
+    end
+
+    return ChanceModule.Run(NewChanceTable) -- This returns the ID.
+end
+
+return StandIDs
